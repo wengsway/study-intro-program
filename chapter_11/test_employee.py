82b4e97e047588ffb0806ad53f927253b495eeab
wengsway <wengsway@outlook.com>
2019-02-22 21:39:17 +0800

    The record of learning chapter 10.

--- chapter_11/test_employee.py
+++ chapter_11/test_employee.py
@@ -17,12 +17,12 @@ class TestEmployee(unittest.TestCase):
         self.my_employee = Employee('weng', 'sway', 10000)
 
     def test_give_default_raise(self):
-        res = self.my_employee.give_raise()
-        self.assertEqual(15000, res)
+        new_salary = self.my_employee.give_raise()
+        self.assertEqual(15000, new_salary)
 
     def test_give_custom_raise(self):
-        res = self.my_employee.give_raise(10000)
-        self.assertEqual(20000, res)
+        new_salary = self.my_employee.give_raise(10000)
+        self.assertEqual(20000, new_salary)
 
 
 if __name__ == '__main__':
